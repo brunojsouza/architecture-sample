4669b4e635dc89b039f9de09aebdd363a1d35f56
Bruno Souza <souza.brunoj@gmail.com>
2020-07-24 18:04:21 -0300

    Remove unused libraries in modules.

--- data/src/main/java/br/com/souzabrunoj/data/util/RequestWrapper.kt
+++ data/src/main/java/br/com/souzabrunoj/data/util/RequestWrapper.kt
@@ -1,22 +1,15 @@
 package br.com.souzabrunoj.data.util
 
 interface RequestWrapper {
-    suspend fun <D> wrapper(
-        call: suspend () -> D
-    ): D
-}
+    suspend fun <D> wrapper(call: suspend () -> D): D }
 
 class RequestWrapperImpl : RequestWrapper {
     @Synchronized
-    override suspend fun <D> wrapper(
-        call: suspend () -> D
-    ): D {
-
+    override suspend fun <D> wrapper(call: suspend () -> D): D {
         return try {
             call()
         } catch (exception: Exception) {
             throw exception
         }
     }
-
 }
